c6db5e4f22a3ed603667f3d802ac8cdbcae3397d
wiryawaneo <35023905+wiryawaneo@users.noreply.github.com>
2023-07-20 19:25:27 +0800

    Add reset.css, basic add book functions and style

--- script.js
+++ script.js
@@ -19,10 +19,22 @@ const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295", "read");
 
 theHobbit.info();
 
+myLibrary.push(theHobbit);
+
 function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
-
   // myLibrary.push(title);
-  // console.log(myLibrary)
+  console.log(myLibrary);
+}
+
+//check whether there are any book input
+//maybe change this into a function so it can run whenever a book is added and stuff
+if (myLibrary.length === 0) {
+  console.log("empty library");
+  console.log(myLibrary.length);
+}
+
+if (myLibrary.length != 0) {
+  console.log("library");
 }
