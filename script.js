ef8db35353343dc20d1214b24ef658ea3de2fa9d
wiryawaneo <35023905+wiryawaneo@users.noreply.github.com>
2023-07-24 17:50:38 +0800

    Call out each book from library array and display

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let myLibrary = [];
 
 //set book object
-function Book(title, author, pages, read) {
-  (this.title = title),
-    (this.author = author),
-    (this.pages = pages),
-    (this.read = read);
+class Book {
+  constructor(title, author, pages, read) {
+    (this.title = title),
+      (this.author = author),
+      (this.pages = pages),
+      (this.read = read);
+  }
 }
 
 //book function
@@ -16,34 +18,39 @@ Book.prototype.info = function () {
 };
 
 const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295", "read");
+const harryPotter = new Book("Harry Potter", "J.R.R Tolkien", "295", "read");
 
-theHobbit.info();
+// theHobbit.info();
 
 myLibrary.push(theHobbit);
+myLibrary.push(harryPotter);
 
-function addBookToLibrary(title, author, pages, read) {
-  const book = new Book(title, author, pages, read);
-  myLibrary.push(book);
-  // myLibrary.push(title);
-  console.log(myLibrary);
-}
-
-showLibraryCards();
-
-//check whether there are any book input
-//maybe change this into a function so it can run whenever a book is added and stuff
-function showLibraryCards() {
-  if (myLibrary.length === 0) {
-    console.log("empty library");
-    console.log(myLibrary.length);
-  }
-
-  if (myLibrary.length != 0) {
-    console.log("library");
-    //get card section
-    const cards = document.getElementsByClassName("library-cards");
-    const card = document.createElement("div");
-    card.classList.add("card");
-    cards[0].appendChild(card);
+class addBookToLibrary {
+  constructor(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    // myLibrary.push(title);
   }
 }
+
+//create each library card
+const createLibraryCard = (book) => {
+  console.log(book);
+  const cards = document.getElementsByClassName("library-cards");
+  const card = document.createElement("div");
+  const cardTitle = document.createElement("div");
+  const cardDelete = document.createElement("button");
+
+  card.classList.add("card");
+  cardDelete.classList.add("card-delete");
+
+  cardDelete.innerHTML = "Delete";
+  cardTitle.innerHTML = book.title;
+
+  card.appendChild(cardDelete);
+  card.appendChild(cardTitle);
+  cards[0].appendChild(card);
+};
+
+//forEach to create a library card for each book in myLibrary array
+myLibrary.forEach(createLibraryCard);
